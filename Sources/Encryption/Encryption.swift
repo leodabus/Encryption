6e3823f27014437b8e32e112b32479d3949be6af
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-10 21:01:12 -0300

    Initial commit

---
+++ Sources/Encryption/Encryption.swift
@@ -0,0 +1,30 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import struct Foundation.Data
+
+extension StringProtocol {
+
+    var data: Data { .init(utf8) }
+    var hexaData: Data { .init(hexa) }
+    var hexaBytes: [UInt8] { .init(hexa) }
+
+    private var hexa: UnfoldSequence<UInt8, Index> {
+        sequence(state: startIndex) { startIndex in
+            guard startIndex < self.endIndex else { return nil }
+            let endIndex = self.index(
+                startIndex,
+                offsetBy: 2,
+                limitedBy: self.endIndex
+            ) ?? self.endIndex
+            defer { startIndex = endIndex }
+            return UInt8(self[startIndex..<endIndex], radix: 16)
+        }
+    }
+}
+
+extension Sequence where Element == UInt8 {
+
+    var string: String? { .init(bytes: self, encoding: .utf8) }
+    var hexa: String { map { String(format: "%02hhx", $0) }.joined() }
+}
---
+++ Sources/Encryption/Linux.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  
+//
+//  Created by lsd on 10/05/24.
+//
+
+#if os(Linux)
+import Vapor
+
+extension StringProtocol {
+
+    var sha256hexa: String { data.sha256digest.hexa }
+    var sha384hexa: String { data.sha384digest.hexa }
+    var sha512hexa: String { data.sha512digest.hexa }
+
+    func sealed<D: ContiguousBytes>(
+        using key: D,
+        nonce: AES.GCM.Nonce? = nil
+    ) throws -> AES.GCM.SealedBox {
+        try AES.GCM.seal(data, using: key, nonce: nonce)
+    }
+
+    func encrypted<D: ContiguousBytes>(using key: D) throws -> Data? {
+        try sealed(using: key).combined
+    }
+}
+
+extension DataProtocol {
+
+    var sha256digest: SHA256Digest { SHA256.hash(data: self) }
+    var sha384digest: SHA384Digest { SHA384.hash(data: self) }
+    var sha512digest: SHA512Digest { SHA512.hash(data: self) }
+
+    func sealedBox() throws -> AES.GCM.SealedBox { try .init(combined: self) }
+
+    func decrypt<D: ContiguousBytes>(using key: D) throws -> Data? {
+        try AES.GCM.open(
+            sealedBox(),
+            using: key
+        )
+    }
+}
+
+
+extension ContiguousBytes {
+    var symmetricKey: SymmetricKey { .init(data: self) }
+}
+
+#endif
