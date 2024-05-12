aa1ed2c112c64992b87472ecedcb857a93daa415
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-12 03:06:22 -0300

    Make them public

--- Sources/Encryption/macOS.swift
+++ Sources/Encryption/macOS.swift
@@ -13,31 +13,31 @@ import CryptoKit
 
 public extension StringProtocol {
 
-    var sha256hexa: String { data.sha256digest.hexa }
-    var sha384hexa: String { data.sha384digest.hexa }
-    var sha512hexa: String { data.sha512digest.hexa }
+    public var sha256hexa: String { data.sha256digest.hexa }
+    public var sha384hexa: String { data.sha384digest.hexa }
+    public var sha512hexa: String { data.sha512digest.hexa }
 
-    func sealed<D: ContiguousBytes>(
+    public func sealed<D: ContiguousBytes>(
         using key: D,
         nonce: AES.GCM.Nonce? = nil
     ) throws -> AES.GCM.SealedBox {
         try AES.GCM.seal(data, using: key.symmetricKey, nonce: nonce)
     }
 
-    func encrypted<D: ContiguousBytes>(using key: D) throws -> Data? {
+    public func encrypted<D: ContiguousBytes>(using key: D) throws -> Data? {
         try sealed(using: key).combined
     }
 }
 
 public extension DataProtocol {
 
-    var sha256digest: SHA256Digest { SHA256.hash(data: self) }
-    var sha384digest: SHA384Digest { SHA384.hash(data: self) }
-    var sha512digest: SHA512Digest { SHA512.hash(data: self) }
+    public var sha256digest: SHA256Digest { SHA256.hash(data: self) }
+    public var sha384digest: SHA384Digest { SHA384.hash(data: self) }
+    public var sha512digest: SHA512Digest { SHA512.hash(data: self) }
 
-    func sealedBox() throws -> AES.GCM.SealedBox { try .init(combined: self) }
+    public func sealedBox() throws -> AES.GCM.SealedBox { try .init(combined: self) }
 
-    func decrypt<D: ContiguousBytes>(using key: D) throws -> Data? {
+    public func decrypt<D: ContiguousBytes>(using key: D) throws -> Data? {
         try AES.GCM.open(
             sealedBox(),
             using: key.symmetricKey
@@ -46,7 +46,7 @@ public extension DataProtocol {
 }
 
 public extension ContiguousBytes {
-    var symmetricKey: SymmetricKey { .init(data: self) }
+    public var symmetricKey: SymmetricKey { .init(data: self) }
 }
 
 #endif
